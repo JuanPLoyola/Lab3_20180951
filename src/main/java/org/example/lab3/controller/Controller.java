87d1a27a44c7118a69b38f9ed24423de74e3ab9a
unknown <eduardo.carbajal@pucp.edu.pe>
2024-04-18 21:14:16 -0500

    ultimo

---
+++ src/main/java/org/example/lab3/controller/Controller.java
@@ -0,0 +1,5 @@
+package org.example.lab3.controller;
+
+@Controller
+public class Controller {
+}
---
+++ src/main/java/org/example/lab3/entity/Shipper.java
@@ -0,0 +1,9 @@
+package org.example.lab3.entity;
+
+
+@Entity
+@table(name = "shippers")
+public class Shipper {
+
+
+}
